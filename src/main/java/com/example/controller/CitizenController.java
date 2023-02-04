ae5b29254bb4cc8f55a999f313fa84ba4ba8dfa0
Sanketbabar2110 <sanketbabar.sb@gmail.com>
2023-02-04 08:53:22 +0530

    first commit

---
+++ src/main/java/com/example/controller/CitizenController.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Citizen;
+import com.example.repository.CitizenRepository;
+
+@RestController
+@RequestMapping("/citizen")
+public class CitizenController {
+	
+	@Autowired
+	CitizenRepository repo;
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<?> getById(@PathVariable Integer id) {
+		List<Citizen> list = repo.findByVaccinationCenterId(id);
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<?> save(@RequestBody Citizen newCitizen) {
+		Citizen citizen = repo.save(newCitizen);
+		return new ResponseEntity<>(citizen, HttpStatus.OK);
+	}
+}
